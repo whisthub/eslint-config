7b2e82096f060c333786fddfd265e3620f231534
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2023-09-22 17:30:55 +0200

    Remove commonjs from environment

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 module.exports = {
 	"env": {
 		"browser": true,
-		"commonjs": true,
-		"es2020": true,
 		"node": true,
 		"mocha": true,
+		"es2022": true,
 	},
 	"extends": [
 		"eslint:recommended",
